96691254365da7478b21d1c7afb183f988cc2195
Elkin Mendoza <elkinmendoza00@gmail.com>
2017-02-23 19:30:23 -0500

    Avances

--- public/scripts/controllers/productCtrl.js
+++ public/scripts/controllers/productCtrl.js
@@ -180,6 +180,35 @@ $overlay.click( function() {
         });
 
       $(document).ready(function() {
+
+
+        if (Modernizr.touch) {
+            // show the close overlay button
+            $(".close-overlay").removeClass("hidden");
+            // handle the adding of hover class when clicked
+            $(".thumbnail").click(function(e){
+                if (!$(this).hasClass("hover")) {
+                    $(this).addClass("hover");
+                }
+            });
+            // handle the closing of the overlay
+            $(".close-overlay").click(function(e){
+                e.preventDefault();
+                e.stopPropagation();
+                if ($(this).closest(".img").hasClass("hover")) {
+                    $(this).closest(".img").removeClass("hover");
+                }
+            });
+        } else {
+            // handle the mouseenter functionality
+            $(".thumbnail").mouseenter(function(){
+                $(this).addClass("hover");
+            })
+            // handle the mouseleave functionality
+            .mouseleave(function(){
+                $(this).removeClass("hover");
+            });
+        }
  
         $('#myCarousel').carousel({
                 interval: 2800
