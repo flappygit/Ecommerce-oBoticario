2cf6abddcfebda4aa159771d7a98fa934ab74ddd
Elkin Mendoza <elkinmendoza00@gmail.com>
2017-02-25 20:20:57 -0500

    Cambio animaciones parallax en home.js

--- public/scripts/controllers/home.js
+++ public/scripts/controllers/home.js
@@ -9,26 +9,25 @@
  */
 angular.module('ecommerceApp')
   .controller('inicioCtrl', function ($scope,$location, $http) {
-       $(function () {
-
+       
 
           function parallax () {
-  var layer_1= document.getElementById('lirio1');
-  var layer_2= document.getElementById('lirio2');
-  var layer_8= document.getElementById('lirio3');
+            var layer_1= document.getElementById('lirio1');
+            var layer_2= document.getElementById('lirio2');
+            var layer_8= document.getElementById('lirio3');
 
-  var layer_3= document.getElementById('pintura1');
-  var layer_4= document.getElementById('pintura2');
-  var layer_6= document.getElementById('pintura3');
+            var layer_3= document.getElementById('pintura1');
+            var layer_4= document.getElementById('pintura2');
+            var layer_6= document.getElementById('pintura3');
 
-  var layer_12= document.getElementById('nativa1');
-  var layer_13= document.getElementById('nativa2');
-  var layer_14= document.getElementById('nativa3');
+            var layer_12= document.getElementById('nativa1');
+            var layer_13= document.getElementById('nativa2');
+            var layer_14= document.getElementById('nativa3');
 
-  var layer_7= document.getElementById('img-product-lily');
-  var layer_9= document.getElementById('img-product-make-b');
-  var layer_10= document.getElementById('img-product-nativa');
-  var layer_11= document.getElementById('img-product-malbec');
+            var layer_7= document.getElementById('img-product-lily');
+            var layer_9= document.getElementById('img-product-make-b');
+            var layer_10= document.getElementById('img-product-nativa');
+            var layer_11= document.getElementById('img-product-malbec');
 
 
               //aplicando possición
@@ -48,31 +47,36 @@ angular.module('ecommerceApp')
 
 
 }
-$(document).scroll(function () {
-    if ($(document).width()>1090) {
-        parallax ();
-
-    }
-});
 
 
+  angular.element(window).ready(function () {
 
-        $(window).ready(function(){
-          
-          if ($(document).width()>1090) {
+    if ($location.path()=="/inicio") {
+
+          if (angular.element(window).width()>1090) {
             parallax ();
 
           }
 
-
-        window.setTimeout(function(){
-            if($(window).width() > 768)
+        angular.element(window).setTimeout(function(){
+            if(angular.element(window).width() > 768)
         {
                     $('#myModal').modal('show');
 
         }
           }, 60000);
-                });
+     }
+  });
+
+
+
+
+
+    $(function () {
+
+
+
+
 
           $(".footer").show();
 
