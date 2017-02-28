57ebfd8989db116d106a6977de98fefd87d96b2d
Christian Lesmes <clesmesc@gmail.com>
2017-02-28 09:40:45 -0500

    Cambios

---
+++ public/scripts/controllers/usuarios_fb.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ecommerceApp.controller:GlobalCtrl
+ * @description
+ * # GlobalCtrl
+ * Controller of the ecommerceApp
+ */
+angular.module('ecommerceApp')
+    .controller('UsuariosFbCtrl', function ($scope, $cookieStore, $location, server,$http) {
+        $http({
+            url: server+'users/',
+            dataType: 'json',
+            method: 'get'
+        })
+            .then(function (request) {
+                console.log(request);
+                if (request.data.success) {
+
+                    $scope.entidad = request.data.message;
+                }
+            })
+            .finally(function () {
+
+
+            });
+
+
+        $scope.cerrarSesionAdmin = function () {
+            $cookieStore.remove('admin');
+            $cookieStore.remove('id_admin');
+            $cookieStore.remove('usuario_admin');
+            $cookieStore.remove('rol_admin');
+            $scope.administrador=false;
+            $location.path('/admin/login');
+        };
+
+    });
