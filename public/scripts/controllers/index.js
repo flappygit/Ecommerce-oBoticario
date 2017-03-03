1449f4fd5966c6155baadb359b93cf57597d079c
ElkinDev <elkinmendoza@gmail.com>
2017-03-03 10:56:00 -0500

    cambios referencias y cambios frontend

--- public/scripts/controllers/index.js
+++ public/scripts/controllers/index.js
@@ -23,7 +23,13 @@ angular.module('ecommerceApp')
 
         //Jquery 
         $(function () {
-        
+        $('.ancla').click(function(e){
+      e.preventDefault();
+        var enlace  = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(enlace).offset().top
+        }, 1000);
+      });
         $(".navbar").hide();
         $("#nav-icon-sticky").hide();
         $(".footer").hide();
